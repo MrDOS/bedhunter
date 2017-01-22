fd1bf1eb1f597eb1714a8f83c52baa72024b4bf5
Samuel Coleman <samuel@seenet.ca>
2017-01-22 16:39:53 -0500

    Run periodically and revisit failures.

--- bedhunter.js
+++ bedhunter.js
@@ -25,69 +25,147 @@ fs.readdirSync(__dirname + '/heuristics').forEach(function(file) {
 });
 
 var db = new sqlite3.Database('bedhunter.db');
-db.run('create table if not exists ad (url text primary key, summary json, details json)');
-db.run('create table if not exists score (url text, heuristic text, score double)');
 
-/**
- * Check Kijiji for new ads. Does not query into them.
- */
-var queryAds = function () {
-    kijiji.query(config.query.prefs, config.query.params, function (err, ads) {
-        if (err !== null) {
-            console.log(err);
-            return;
-        }
+var defineSchema = function () {
+    return new Promise(function (resolve) {
+        console.log('Defining database schema...');
 
-        var stmt = db.prepare('insert into ad (url, summary) values (?, ?)');
-        ads.forEach(function (ad) {
-            stmt.run(ad.link, JSON.stringify(ad), function (err) {
-                if (err != null)
-                {
-                    return;
-                }
+        db.run('create table if not exists ad (link text primary key, summary json, details json)', function () {
+            db.run('create table if not exists score (link text, heuristic text, score double, foreign key (link) references ad (link))', function () {
+                db.run('drop table if exists heuristic', function () {
+                    db.run('create table heuristic (heuristic text primary key)', function () {
+                        var stmt = db.prepare('insert into heuristic (heuristic) values (?)');
+                        var inserted = 0;
 
-                scrapeAd(ad);
+                        var heuristicKeys = Object.keys(heuristics);
+                        heuristicKeys.forEach(function (heuristic) {
+                            stmt.run(heuristic, function () {
+                                if (++inserted === heuristicKeys.length) {
+                                    console.log('Defined heuristics in database.');
+                                    stmt.finalize();
+                                    resolve();
+                                }
+                            });
+                        });
+                    });
+                });
             });
         });
     });
 };
 
-var scrapeAd = function (ad) {
-    var stmt = db.prepare('update ad set details = ? where url = ?');
+/**
+ * Check Kijiji for new ads. Does not query into them.
+ */
+var queryAdsIntoDatabase = function () {
+    return new Promise(function (resolve) {
+        console.log('Querying latest ads...');
 
-    kijiji.scrape(ad.link, function (err, details) {
-        if (err != null) {
-            console.log(err);
-            return;
-        }
-
-        ad.innerAd = details;
-        stmt.run(JSON.stringify(details), ad.link, function () {
-            if (err != null)
-            {
+        kijiji.query(config.query.prefs, config.query.params, function (err, ads) {
+            if (err !== null) {
                 console.log(err);
-                return;
-            }
+                resolve();
+            } else if (ads.length == 0) {
+                console.log('Kijiji returned no ads.');
+                resolve();
+            } else {
+                var stmt = db.prepare('insert into ad (link, summary) values (?, ?)');
+                var inserted = 0;
 
-            scoreAd(ad);
+                ads.forEach(function (ad) {
+                    stmt.run(ad.link, JSON.stringify(ad), function () {
+                        if (++inserted === ads.length) {
+                            console.log('Retrieved latest ads.');
+                            stmt.finalize();
+                            resolve();
+                        }
+                    });
+                });
+            }
         });
     });
 };
 
-var scoreAd = function (ad) {
-    var stmt = db.prepare('insert into score (url, heuristic, score) values (?, ?, ?)');
+var scrapeAllUnscrapedAds = function () {
+    return new Promise(function (resolve) {
+        console.log('Retrieving ad details...');
 
-    for (heuristic in heuristics) {
-        heuristics[heuristic](ad, function (err, score) {
-            if (err != null) {
+        db.all('select link, summary from ad where details is null', function (err, ads) {
+            if (err != null)
+            {
                 console.log(err);
-                return;
-            }
+                resolve();
+            } else if (ads.length == 0) {
+                console.log('No ads without details.');
+                resolve();
+            } else {
+                var stmt = db.prepare('update ad set details = ? where link = ?');
+                var inserted = 0;
 
-            console.log(ad.title + ' -> ' + heuristic + ': ' + score + '/100');
-            stmt.run(ad.link, heuristic, score);
+                ads.forEach(function (ad) {
+                    kijiji.scrape(ad.link, function (err, details) {
+                        if (err == null) {
+                            ad.innerAd = details;
+                            stmt.run(JSON.stringify(details), ad.link);
+                        }
+
+                        if (++inserted === ads.length) {
+                            console.log('Retrieved ad details.');
+                            stmt.finalize();
+                            resolve();
+                        }
+                    });
+                });
+            }
         });
-    }
+    });
 };
 
-queryAds();
+var scoreAllAdsMissingScores = function () {
+    return new Promise(function (resolve) {
+        console.log('Scoring ads...');
+
+        db.all('select heuristic.heuristic, ad.summary, ad.details from heuristic left join ad left join score on score.link = ad.link where score.score is null', function (err, rows) {
+            if (err !== null) {
+                console.log(err);
+                resolve();
+            } else if (rows.length == 0) {
+                console.log('No unscored ads.');
+                resolve();
+            } else {
+                var stmt = db.prepare('insert into score (link, heuristic, score) values (?, ?, ?)');
+                var inserted = 0;
+
+                rows.forEach(function (row) {
+                    var ad = JSON.parse(row.summary);
+                    ad.innerAd = JSON.parse(row.details);
+
+                    heuristics[row.heuristic](ad, function (err, score) {
+                        if (err == null) {
+                            stmt.run(ad.link, row.heuristic, score);
+                        }
+
+                        if (++inserted === rows.length) {
+                            console.log('Scored ads.');
+                            stmt.finalize();
+                            resolve();
+                        }
+                    });
+                });
+
+                resolve();
+            }
+        });
+    });
+};
+
+var process = function ()
+{
+    queryAdsIntoDatabase()
+        .then(() => scrapeAllUnscrapedAds())
+        .then(() => scoreAllAdsMissingScores())
+        .then(() => setTimeout(process, config.query.interval));
+}
+
+defineSchema()
+    .then(process);
