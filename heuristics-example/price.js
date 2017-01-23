28c9059fd85fb57ca41fefcc1d659717c565de57
Samuel Coleman <samuel@seenet.ca>
2017-01-23 01:00:56 -0500

    Fix “Please Contact” price scoring.

--- heuristics-example/price.js
+++ heuristics-example/price.js
@@ -2,7 +2,12 @@ var GOAL_PRICE = 800;
 var DEDUCTION_PRICE = 10;
 
 exports.price = function (ad, callback) {
-    var price = parseFloat(ad['g-core:price']);
-    var score = Math.max(0, 100 - Math.max(price - GOAL_PRICE, 0) / DEDUCTION_PRICE);
-    callback(null, score);
+    /* If the price isn't given, who cares about it. */
+    if (ad['g-core:price'] === undefined) {
+        callback(null, 0);
+    } else {
+        var price = parseFloat(ad['g-core:price']);
+        var score = Math.max(0, 100 - Math.max(price - GOAL_PRICE, 0) / DEDUCTION_PRICE);
+        callback(null, score);
+    }
 };
